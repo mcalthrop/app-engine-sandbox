a2ba2b4f0596f2ad3df8308732946cd92beaf89e
Matt Calthrop <mcalthrop@gmail.com>
2012-11-19 13:08:20 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('this is the day your life will surely change')
+        self.response.write('this is the day<br>your life will surely change')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
